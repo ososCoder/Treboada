ff881b20920aa0c53b82a447e7d07e4c64be6876
oscar_ousinde <oscar.ousinde@hotmail.com>
2022-11-26 20:47:44 +0100

    API calls

---
+++ js/mainOpenW.js
@@ -0,0 +1,59 @@
+//OPEN WEATHER KEY/////////////////////////////////////////////////////////////////////////////////////////////////////////
+const KEY = '7e44fe40f6070e033fef8c5210183815';
+
+//API CALL URL/////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//api.openweathermap.org/data/2.5/forecast?lat={lat}&lon={lon}&appid={API key} --> forecast
+//http://api.openweathermap.org/geo/1.0/direct?q=${municipio}&limit=1&appid=${KEY} --> geocoding
+//https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={API key} --> current weather
+
+//API WEATHER CALL (current)///////////////////////////////////////////////////////////////////////////////////////////////
+const getCurrentWeather = async (latitude, longitude) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&units=metric&lang=es&appid=${KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//API WEATHER CALL (forecast)//////////////////////////////////////////////////////////////////////////////////////////////
+const getForecastWeather = async (latitude, longitude) => {
+  try {
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&units=metric&lang=es&appid=${KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//GEOCODING CALL///////////////////////////////////////////////////////////////////////////////////////////////////////////
+const apiCallGeocoding = async (municipio) => {
+  try {
+    const response = await fetch(
+      `http://api.openweathermap.org/geo/1.0/direct?q=${municipio}&limit=1&appid=${KEY}`
+    );
+    const data = await response.json();
+    console.log(data);
+
+    const { lat, lon } = data[0];
+    console.log(lat);
+    console.log(lon);
+
+    //llamada a la función con los argumentos latitude & longitude
+    await getForecastWeather(lat, lon);
+    await getCurrentWeather(lat, lon);
+  } catch (err) {
+    console.error(err);
+  }
+};
+
+//esta llamada a la función de geocoding tendrá que ir dentro del eventlistener del button
+//y se puede crear otro evento keydown para llamar a la APi EL tiempo con un array de municipios
+//para hacer el autocomplete
+apiCallGeocoding('coristanco');
